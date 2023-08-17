1610bb9a8b61ce2cacc57bc20d8276c8ff7cdf76
Mackenzie <chibumackie@gmail.com>
2023-08-17 13:55:21 +0100

    Added my project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+   runApp(
+    const Home(),
+  );
+}
+ class Home extends StatelessWidget {
+   const Home({Key? key}) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+     return Container();
+   }
+ }
