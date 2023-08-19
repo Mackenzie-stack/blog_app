00ff6fe33be4d2dc83262b64cc73911403ce8bef
Mackenzie <chibumackie@gmail.com>
2023-08-19 14:57:19 +0100

    Blog is now displaying both picture and description

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,8 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
 
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,156 +10,161 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final String apiKey = 'AIzaSyC7ENUcXTLoTO6WpS5RZ0YJHnhEjTNSsQI';
-  final String blogId = '4776826234817506983';
-
-  List<dynamic> posts = [];
-
-  @override
+  var _isLoading = true; //For progress bar
+  var posts;
+  var imgUrl;
+  //initialization
   void initState() {
     super.initState();
-    fetchBlogPosts();
+    _fetchData();
   }
-
-  Future<void> fetchBlogPosts() async {
-    final response = await http.get(Uri.parse(
-        'https://www.googleapis.com/blogger/v3/blogs/$blogId/posts?key=$apiKey'));
-
+  //Function to fetch data from JSON
+  @override
+  _fetchData() async {
+    print("attempting");
+    final String url =
+        "https://www.googleapis.com/blogger/v3/blogs/4776826234817506983/posts/"
+        "?key=AIzaSyC7ENUcXTLoTO6WpS5RZ0YJHnhEjTNSsQI";
+    final response = await http.get(Uri.parse(url)) ;
+    print(response);
     if (response.statusCode == 200) {
+      //HTTP OK is 200
+      final Map items = json.decode(response.body);
+      var post = items['items'];
+
       setState(() {
-        posts = json.decode(response.body)['items'];
+        _isLoading = false;
+        this.posts = post;
       });
-    } else {
-      print('Failed to fetch blog posts');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Blog Posts'),
-      ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: (context, index) {
-          var imageUrl = posts[index]['author']['image']['url']; // Assuming images are stored in the 'images' array
-print(posts[index]['author']);
-          return
-            ListTile(
-             // leading: Image.network(imageUrl),
-            title: Text(posts[index]['title']),
-            trailing: Text(posts[index]['author']['displayName']),
-          );
-        },
-      ),
-    );
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Blogger"),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.refresh),
+                onPressed: () {
+                  setState(() {
+                    _isLoading = true;
+                  });
+                  _fetchData();
+                })
+          ],
+        ),
+        body: new Center(
+            child: _isLoading
+                ? new CircularProgressIndicator()
+                : ListView.builder(
+              itemCount: this.posts != null ? this.posts.length : 0,
+              itemBuilder: (context, i) {
+                final Post = this.posts[i];
+                final postDesc = Post["content"];
+                //All the below code is to fetch the image
+                var document = parse(postDesc);
+                //Regular expression
+                RegExp regExp = new RegExp(
+                  r"(https?:\/\/.*\.(?:png|jpg|))",
+                  caseSensitive: false,
+                  multiLine: false,
+
+                );
+                final match = regExp
+                    .stringMatch(document.outerHtml.toString())
+                    .toString();
+                print(document.outerHtml);
+                //print(document.outerHtml);
+                //print("firstMatch : " + match);
+                //Converting the regex output to image (Slashing) , since the output from regex was not perfect for me
+                if (match.length > 5) {
+                  if (match.contains(".png")) {
+                    imgUrl = match.substring(0, match.indexOf(".png"));
+                    print(imgUrl);
+                  } else if (match.contains(".jpg")){
+                    imgUrl = match.substring(0, match.indexOf(".jpg"));
+
+                  }
+
+                  else {
+                    imgUrl =
+                    "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgFlQKvHKgpMqFXOrISpzrHMIbObF2iumQWufn3BzOkSV05bU0VxFTug285zBAPriJKorw_O5HiZauGifviH4KJiqtv_Znza_unj_Q1CyGb0eN_aLMj5cujOSTetTs066nR4IjBCam3PcsjK-4QwkB2tUwJfDX8h6O9qOTsDfXQhngQLCbeM-4pyzchxw/w640-h360/grow%20your%20business%20with%20your%20personal%20profile.png";
+                  }
+                }
+                String description = document.body!.text.trim();
+                //print(description);
+
+                return new Container(
+                  padding:
+                  const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        width: 500.0,
+                        height: 180.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              //check if the image is not null (length > 5) only then check imgUrl else display default img
+                              image: new NetworkImage(imgUrl
+                                  .toString()
+                                  .length >
+                                  10
+                                  ? imgUrl.toString()
+                                  : "https://blogger.googleusercontent.com/img/b/R29vZ2xl/"
+                "AVvXsEgFlQKvHKgpMqFXOrISpzrHMIbObF2iumQWufn3BzOkSV05bU0VxFTug285zBAPriJKorw_O"
+                "5HiZauGifviH4KJiqtv_Znza_unj_Q1CyGb0eN_aLMj5cujOSTetTs066nR4IjBCam3PcsjK-"
+                "4QwkB2tUwJfDX8h6O9qOTsDfXQhngQLCbeM-4pyzchxw/w640-h360/grow%20your%20business"
+                "%20with%20your%20personal%20profile.png")),
+                        ),
+                      ),
+                      new Padding(
+                        padding:
+                        const EdgeInsets.symmetric(vertical: 10.0),
+                        child: new Text(
+                          Post["title"],
+                          maxLines: 3,
+                          style: new TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      new Text(
+                        description.replaceAll("\n", ", "),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: new TextStyle(fontSize: 15.0),
+                      ),
+                      new Padding(
+                        padding:
+                        const EdgeInsets.symmetric(vertical: 16.0),
+                        child: new TextButton(
+                          child: new Text("READ MORE",style: new TextStyle(color: Colors.white),),
+                          onPressed: ()
+                          {}
+                          // {
+                          //   //We will pass description to postview through an argument
+                          //   Navigator
+                          //       .of(context)
+                          //       .push(new MaterialPageRoute<Null>(
+                          //     builder: (BuildContext context) {
+                          //       return PostView(Post['title'],description,imgUrl);
+                          //     },
+                          //   ));
+                          // },
+                        ),
+                      ),
+                      Divider(),
+                    ],
+                  ),
+                );
+              },
+            )));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'dart:convert';
-//
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:blog_app/models//api_key.dart' as Constant;
-// import '../models/blog_all.dart';
-// import '../models/blog_items.dart';
-//
-//
-//
-// Future<PostList> fetchPosts() async {
-//   var postListUrl = Uri.https(
-//       "blogger.googleapis.com", "/v3/blogs/4776826234817506983/posts/", {"key": 'AIzaSyC7ENUcXTLoTO6WpS5RZ0YJHnhEjTNSsQI'});
-//   final response = await http.get(postListUrl);
-//   if (response.statusCode == 200) {
-//     return PostList.fromJson(jsonDecode(response.body));
-//   } else {
-//     throw Exception();
-//   }
-// }
-//
-//
-// Future<PostList> fetchAlbum() async {
-//   final response = await http
-//       .get(Uri.parse('https://www.googleapis.com/blogger/v3/blogs/4776826234817506983?key'
-//       '=AIzaSyC7ENUcXTLoTO6WpS5RZ0YJHnhEjTNSsQI'));
-//
-//   if (response.statusCode == 200) {
-//     // If the server did return a 200 OK response,
-//     // then parse the JSON.
-//     print("Successful");
-//     return PostList.fromJson(jsonDecode(response.body));
-//   } else {
-//     // If the server did not return a 200 OK response,
-//     // then throw an exception.
-//     throw Exception('Failed to load album');
-//   }
-// }
-//
-// class HomePage extends StatefulWidget {
-//   const HomePage({Key? key}) : super(key: key);
-//
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-//
-// class _HomePageState extends State<HomePage> {
-//   late Future<PostList> futureAlbum;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     futureAlbum = fetchAlbum();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('My Blog'),
-//       ),
-//       body: FutureBuilder(
-//         // : null,
-//           future: fetchPosts(),
-//           builder: (context, snapshot) {
-//             if (!snapshot.hasData) {
-//               return Center(
-//                 child: CircularProgressIndicator(),
-//               );
-//             } else
-//               return ListView.builder(
-//                 shrinkWrap: true,
-//                 itemCount: snapshot.data!.posts.length ?? 1,
-//                 itemBuilder: (context, index) {
-//                   return Card(
-//                     child: ListTile(
-//                       title: Text(
-//                         snapshot.data .posts[index].title ?? "no items",
-//                       ),
-//                       subtitle: Text(
-//                           snapshot.data.posts[index].author.displayName ??
-//                               "No Auther"),
-//                     ),
-//                   );
-//                 },
-//               );
-//           }),
-//     );
-//
-//     );
-//   }
-// }
